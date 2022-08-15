6506833dd0970280809e9868b55976a52a84f963
AsametDogan <smt.3000.dgn@gmail.com>
2022-08-15 23:08:50 +0300

    json update

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL:process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_ID
+    apiKey: "AIzaSyDZg-4fp6ad1s0msDEzzHtmxSMhu3A548I",
+    authDomain: "manage-process-app.firebaseapp.com",
+    databaseURL: "gs://manage-process-app.appspot.com",
+    projectId: "manage-process-app",
+    storageBucket:"manage-process-app.appspot.com",
+    messagingSenderId: "347368006183",
+    appId: "1:347368006183:web:e3aca49c47d69ada976402"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
